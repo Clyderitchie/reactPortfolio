7d474246e772d778b0edc535b4ae46340bdbb3f8
francis <clyderitchie@yahoo.com>
2024-01-20 22:06:07 -0800

    Going to work on some of the built in stuff I would like to add to the application

---
+++ portfolio/src/pages/projects/haze.jsx
@@ -0,0 +1,36 @@
+
+function Haze() {
+    return (
+        <>
+            <div id="hello" className="container mt-5 text-light">
+                <div className="row d-flex justify-content-center">
+                <div className="col-6">
+                        <div className="card mb-3 bg-dark">
+                            <img src="https://placehold.co/600x400" className="card-img-top" alt="Screenshot of Haze" />
+                            <div className="card-body">
+                                <h5 className="text-light text-center">Haze</h5>
+                                <p className="text-light text-center">The Steam clone you didn't ask for, but the one you needed.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row ">
+                    <div className="col-12 border d-flex justify-content-start">
+                        <h2 className="text-light">
+                           Overview:
+                        </h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6 border">
+                        <h3 className="text-light">Haze</h3>
+                        <p className="text-light">This application was built in order to</p>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+};
+
+export default Haze;
--- portfolio/src/pages/projects/projectsPage.jsx
+++ portfolio/src/pages/projects/projectsPage.jsx
@@ -1,4 +1,4 @@
-
+import Haze from './haze';
 
 function Projects() {
     return (
@@ -9,8 +9,11 @@ function Projects() {
                         <div className="card mb-3 bg-dark">
                             <img src="https://placehold.co/600x400" className="card-img-top" alt="Screenshot of Haze" />
                             <div className="card-body">
-                                <h5 className="text-light">Haze</h5>
-                                <p className="text-light">A clone of the popular gaming site Steam.</p>
+                                <a className="text-decoration-none" href="https://haze-community-a1679c79f3fa.herokuapp.com/" target="_blank">
+                                <h5 className="text-light text-center">Haze</h5>
+                                <p className="text-light text-center">A clone of the popular gaming site Steam.</p>
+                                <p className="text-light text-center">A Full Stack application that was built using <br />the CRUD method, with MySql, express, and express handlebars.</p>
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -18,8 +21,8 @@ function Projects() {
                         <div className="card mb-3 bg-dark">
                             <img src="https://placehold.co/600x400" className="card-img-top" alt="Screenshot of Haze" />
                             <div className="card-body">
-                                <h5 className="text-light">Dexperience</h5>
-                                <p className="text-light">Welcome to new version of the Pokedex.</p>
+                                <h5 className="text-light text-center">Dexperience</h5>
+                                <p className="text-light text-center">Welcome to new version of the Pokedex.</p>
                             </div>
                         </div>
                     </div>
