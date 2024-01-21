faccbc920a05eb7fc0c526049f31bfa00299fa20
francis <clyderitchie@yahoo.com>
2024-01-20 21:18:07 -0800

    Added in place holder images for the project page cards for the time being

--- portfolio/src/pages/projects/projectsPage.jsx
+++ portfolio/src/pages/projects/projectsPage.jsx
@@ -4,16 +4,26 @@ function Projects() {
     return (
         <>
             <div className="container mt-5">
-                <div className="row ">
-                    <div className="col-12 d-flex justify-content-center">
+                <div className="row d-flex justify-content-between flex-wrap">
+                    <div className="col-6">
                         <div className="card mb-3 bg-dark">
-                            <img src="../img/haze.jpeg" className="card-img-top" alt="Screenshot of Haze"  />
+                            <img src="https://placehold.co/600x400" className="card-img-top" alt="Screenshot of Haze" />
                             <div className="card-body">
                                 <h5 className="text-light">Haze</h5>
                                 <p className="text-light">A clone of the popular gaming site Steam.</p>
                             </div>
                         </div>
                     </div>
+                    <div className="col-6">
+                        <div className="card mb-3 bg-dark">
+                            <img src="https://placehold.co/600x400" className="card-img-top" alt="Screenshot of Haze" />
+                            <div className="card-body">
+                                <h5 className="text-light">Dexperience</h5>
+                                <p className="text-light">Welcome to new version of the Pokedex.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </>
