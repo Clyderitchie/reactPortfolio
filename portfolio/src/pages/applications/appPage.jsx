86414e3fa46546b0c32126b7f3da9a0f72e5b9e0
francis <clyderitchie@yahoo.com>
2024-01-22 21:31:13 -0800

    Styling the pages with some css

--- portfolio/src/pages/applications/appPage.jsx
+++ portfolio/src/pages/applications/appPage.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import AppList from '../../components/ui/appPage/appList';
+import FizzBuzzImage from '../../components/ui/appPage/img/fizzBuzz.png';
+import PasswordGeneratorImage from '../../components/ui/appPage/img/passwordGenerator.png'
 
 
 
@@ -8,13 +10,38 @@ function Apps() {
         <AppList
             links={[
                 <Link key={1} className="list-item text-decoration-none fs-3 text-light" to='/fizzBuzz'>
-                    FizzBuzz
+                    <div className="col-6">
+                        <div className="card mb-3 bg-dark">
+                            <img src={FizzBuzzImage} className="card-img-top" alt="Screenshot of fizzBuzz page" />
+                            <div className="card-body">
+                                    <h5 className="text-light text-center">FizzBuzz</h5>
+                                    <p className="text-light text-center">A new take on a programming classic.</p>
+                            </div>
+                        </div>
+                    </div>
                 </Link>,
                 <Link key={2} className="list-item text-decoration-none fs-3 text-light" to='/calculator'>
-                    Calculator
+                    <div className="col-6">
+                        <div className="card mb-3 bg-dark">
+                            <img src={PasswordGeneratorImage} className="card-img-top" alt="Screenshot of password generator page" />
+                            <div className="card-body">
+                                    <h5 className="text-light text-center">Calculator</h5>
+                                    <p className="text-light text-center">Need a help with some numbers? Well don't worry anymore!</p>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </Link>,
                 <Link key={3} className="list-item text-decoration-none fs-3 text-light" to='/password'>
-                    Password Generator
+                       <div className="col-6">
+                        <div className="card mb-3 bg-dark">
+                            <img src={PasswordGeneratorImage} className="card-img-top" alt="Screenshot of password generator page" />
+                            <div className="card-body">
+                                    <h5 className="text-light text-center">Password Generator</h5>
+                                    <p className="text-light text-center">Tired of trying to create your own unique password? Give this a shot then!</p>
+                            </div>
+                        </div>
+                    </div>
                 </Link>
             ]}
         />
--- portfolio/src/pages/applications/calculator.jsx
+++ portfolio/src/pages/applications/calculator.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import './calculator.css';
 
 function Calculator() {
     const [displayValue, setDisplayValue] = useState('0');
@@ -110,51 +111,51 @@ function Calculator() {
     return (
         <>
             <div className="container mt-5">
-                <div className="row d-flex flex-column justify-content-center">
-                    <div className="col-6">
-                        <div className="calculator card">
+                <div id="calc-container" className="row d-flex flex-column justify-content-center">
+                    <div className="col-3">
+                        <div id="input-space" className="calculator card">
                             <input type="text" className="calculator-screen z-depth-1" value={displayValue} readOnly />
                         </div>
                     </div>
                     <div className="col-6 w-100">
                         <div className="calculator-keys">
-                            <button type="button" className="operator btn btn-info" value="+" onClick={handleButtonClick}>+</button>
-                            <button type="button" className="operator btn btn-info" value="-" onClick={handleButtonClick}>-</button>
-                            <button type="button" className="operator btn btn-info" value="*" onClick={handleButtonClick}>&times;</button>
-                            <button type="button" className="operator btn btn-info" value="/" onClick={handleButtonClick}>&divide;</button>
+                            <button type="button" className="operator btn btn-info oppKeys" value="+" onClick={handleButtonClick}>+</button>
+                            <button type="button" className="operator btn btn-info oppKeys" value="-" onClick={handleButtonClick}>-</button>
+                            <button type="button" className="operator btn btn-info oppKeys" value="*" onClick={handleButtonClick}>&times;</button>
+                            <button type="button" className="operator btn btn-info oppKeys" value="/" onClick={handleButtonClick}>&divide;</button>
                         </div>
                     </div>
                     <div className="col-6">
                         <div className="calculator-keys">
-                            <button type="button" value="7" className="btn btn-light wave-effect" onClick={handleButtonClick}>7</button>
-                            <button type="button" value="8" className="btn btn-light wave-effect" onClick={handleButtonClick}>8</button>
-                            <button type="button" value="9" className="btn btn-light wave-effect" onClick={handleButtonClick}>9</button>
+                            <button type="button" value="7" className="btn btn-light wave-effect numericKeys" onClick={handleButtonClick}>7</button>
+                            <button type="button" value="8" className="btn btn-light wave-effect numericKeys" onClick={handleButtonClick}>8</button>
+                            <button type="button" value="9" className="btn btn-light wave-effect numericKeys" onClick={handleButtonClick}>9</button>
                         </div>
                     </div>
                     <div className="col-6">
                         <div className="calculator-keys">
-                            <button type="button" value="4" className="btn btn-light wave-effect" onClick={handleButtonClick}>4</button>
-                            <button type="button" value="5" className="btn btn-light wave-effect" onClick={handleButtonClick}>5</button>
-                            <button type="button" value="6" className="btn btn-light wave-effect" onClick={handleButtonClick}>6</button>
+                            <button type="button" value="4" className="btn btn-light wave-effect numericKeys" onClick={handleButtonClick}>4</button>
+                            <button type="button" value="5" className="btn btn-light wave-effect numericKeys" onClick={handleButtonClick}>5</button>
+                            <button type="button" value="6" className="btn btn-light wave-effect numericKeys" onClick={handleButtonClick}>6</button>
                         </div>
                     </div>
                     <div className="col-6">
                         <div className="calculator-keys">
-                            <button type="button" value="1" className="btn btn-light wave-effect" onClick={handleButtonClick}>1</button>
-                            <button type="button" value="2" className="btn btn-light wave-effect" onClick={handleButtonClick}>2</button>
-                            <button type="button" value="3" className="btn btn-light wave-effect" onClick={handleButtonClick}>3</button>
+                            <button type="button" value="1" className="btn btn-light wave-effect numericKeys" onClick={handleButtonClick}>1</button>
+                            <button type="button" value="2" className="btn btn-light wave-effect numericKeys" onClick={handleButtonClick}>2</button>
+                            <button type="button" value="3" className="btn btn-light wave-effect numericKeys" onClick={handleButtonClick}>3</button>
                         </div>
                     </div>
                     <div className="col-6">
                         <div className="calculator-keys">
-                            <button type="button" value="0" className="btn btn-light waves-effect" onClick={handleButtonClick}>0</button>
-                            <button type="button" className="decimal function btn btn-secondary" value="." onClick={handleButtonClick}>.</button>
-                            <button type="button" className="all-clear function btn btn-danger btn-sm" value="all-clear" onClick={handleButtonClick}>AC</button>
+                            <button type="button" value="0" className="btn btn-light waves-effect numericKeys" onClick={handleButtonClick}>0</button>
+                            <button type="button" className="decimal function btn btn-secondary numericKeys" value="." onClick={handleButtonClick}>.</button>
+                            <button type="button" className="all-clear function btn btn-danger numericKeys" value="all-clear" onClick={handleButtonClick}>AC</button>
                         </div>
                     </div>
                     <div className="col-6">
                         <div className="calculator-keys">
-                        <button type="button" className="equal-sign operator btn btn-default text-dark bg-light" value="=" onClick={handleButtonClick}>=</button>
+                        <button type="button" className="equal-sign operator btn btn-default text-dark bg-light equalKey" value="=" onClick={handleButtonClick}>=</button>
                         </div>
                     </div>
                 </div>
