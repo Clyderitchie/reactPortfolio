fc33fe7a3e71d2af71ff2a74a50c09cf84974ba2
francis <clyderitchie@yahoo.com>
2024-01-25 17:50:42 -0800

    Deploying

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,7 +3,7 @@ import HomeContent from '../components/ui/homepage/homeContent';
 
 import FizzBuzz from '../components/ui/appPage/img/fizzBuzz.png';
 import Projects from '../components/ui/homepage/img/projectPage.png';
-import Resume from '../components/ui/homepage/img/resumePlaceHolder.png';
+import Resume from '../components/ui/homepage/img/resumePlaceholder.png';
 import Contact from '../components/ui/homepage/img/contactPage.png';
 
 import'../components/ui/homepage/homepage.css';
